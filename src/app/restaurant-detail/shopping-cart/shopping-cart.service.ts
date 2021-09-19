85ef233cf41e8e6e4d4217932b9adcd55bc24728
Arthur Freitas <arthurssfreitas@gmail.com>
2021-09-19 10:09:39 -0300

    modifications

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,9 +1,14 @@
+import { Injectable } from "@angular/core"
+import { NotificationService } from "app/shared/messages/notification.service"
 import { IMenuItem } from "../menu-item/menu-item.model"
 import { CartItem } from "./cart-item.model"
 
 
+@Injectable()
 export class ShoppingCartService{
   items: CartItem[] = []
+
+  constructor(private notificationService: NotificationService){}
 
   clear(){
     this.items = []
@@ -16,9 +21,11 @@ export class ShoppingCartService{
     }else{
       this.items.push(new CartItem(item))
     }
+    this.notificationService.notify(`Você adicionou o item ${item.name}`)
   }
   removeItem(item:CartItem){
     this.items.splice(this.items.indexOf(item), 1)
+    this.notificationService.notify(`Você removeu o item ${item.menuItem.name}`)
   }
 
   total(): number{
